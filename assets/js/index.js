142329d13e6527fdb0b975fb40ef9eab0f5b5a04
CaioAbra <caioabra41@gmail.com>
2024-10-04 01:04:28 -0300

    nvl lendario

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,11 +6,13 @@ $(document).ready(function () {
             $('.nvlOuro').attr('src', './src/img/cards/nvlOuroMobi.png');
             $('.nvlDima').attr('src', './src/img/cards/nvlDimaMobi.png');
             $('.nvlMestre').attr('src', './src/img/cards/nvlMestreMobi.png');
+            $('.nvlLendario').attr('src', './src/img/cards/nvlLendarioMobi.png');
 
             $('.premioUm').attr('src', './src/img/cards/premioUmMob.png');
             $('.premioDois').attr('src', './src/img/cards/premioOuroMobi.png');
             $('.premioDima').attr('src', './src/img/cards/premioDimaMobi.png');
             $('.premioMestre').attr('src', './src/img/cards/premioMestreMobi.png');
+            $('.premioLendario').attr('src', './src/img/cards/premioLendarioMobi.png');
 
 
 
@@ -19,11 +21,13 @@ $(document).ready(function () {
             $('.nvlOuro').attr('src', './src/img/cards/nvlOuro.png');
             $('.nvlDima').attr('src', './src/img/cards/nvlDima.png');
             $('.nvlMestre').attr('src', './src/img/cards/nvlMestre.png');
+            $('.nvlLendario').attr('src', './src/img/cards/nvlLendario.png');
 
             $('.premioUm').attr('src', './src/img/cards/premioUm.png');
             $('.premioDois').attr('src', './src/img/cards/premioOuro.png');
             $('.premioDima').attr('src', './src/img/cards/premioDima.png');
             $('.premioMestre').attr('src', './src/img/cards/premioMestre.png');
+            $('.premioLendario').attr('src', './src/img/cards/premioLendario.png');
 
         }
     }
