117257a5d5bade809dae5bc82957d371881cc014
CaioAbra <caioabra41@gmail.com>
2024-10-04 11:49:02 -0300

    ajuste de img

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,32 +2,32 @@ $(document).ready(function () {
     // Função para verificar a largura da tela e trocar a imagem
     function trocarImagemPorLargura() {
         if ($(window).width() < 1024) {
-            $('.nvlPrata').attr('src', './src/img/cards/nvlPrataMobi.png');
-            $('.nvlOuro').attr('src', './src/img/cards/nvlOuroMobi.png');
-            $('.nvlDima').attr('src', './src/img/cards/nvlDimaMobi.png');
-            $('.nvlMestre').attr('src', './src/img/cards/nvlMestreMobi.png');
-            $('.nvlLendario').attr('src', './src/img/cards/nvlLendarioMobi.png');
+            $('.nvlPrata').attr('src', './src/img/cards/nvlPrataMobi.webp');
+            $('.nvlOuro').attr('src', './src/img/cards/nvlOuroMobi.webp');
+            $('.nvlDima').attr('src', './src/img/cards/nvlDimaMobi.webp');
+            $('.nvlMestre').attr('src', './src/img/cards/nvlMestreMobi.webp');
+            $('.nvlLendario').attr('src', './src/img/cards/nvlLendarioMobi.webp');
 
-            $('.premioUm').attr('src', './src/img/cards/premioUmMob.png');
-            $('.premioDois').attr('src', './src/img/cards/premioOuroMobi.png');
-            $('.premioDima').attr('src', './src/img/cards/premioDimaMobi.png');
-            $('.premioMestre').attr('src', './src/img/cards/premioMestreMobi.png');
-            $('.premioLendario').attr('src', './src/img/cards/premioLendarioMobi.png');
+            $('.premioUm').attr('src', './src/img/cards/premioUmMob.webp');
+            $('.premioDois').attr('src', './src/img/cards/premioOuroMobi.webp');
+            $('.premioDima').attr('src', './src/img/cards/premioDimaMobi.webp');
+            $('.premioMestre').attr('src', './src/img/cards/premioMestreMobi.webp');
+            $('.premioLendario').attr('src', './src/img/cards/premioLendarioMobi.webp');
 
 
 
         } else {
-            $('.nvlPrata').attr('src', './src/img/cards/prata.png');
-            $('.nvlOuro').attr('src', './src/img/cards/nvlOuro.png');
-            $('.nvlDima').attr('src', './src/img/cards/nvlDima.png');
-            $('.nvlMestre').attr('src', './src/img/cards/nvlMestre.png');
-            $('.nvlLendario').attr('src', './src/img/cards/nvlLendario.png');
+            $('.nvlPrata').attr('src', './src/img/cards/prata.webp');
+            $('.nvlOuro').attr('src', './src/img/cards/nvlOuro.webp');
+            $('.nvlDima').attr('src', './src/img/cards/nvlDima.webp');
+            $('.nvlMestre').attr('src', './src/img/cards/nvlMestre.webp');
+            $('.nvlLendario').attr('src', './src/img/cards/nvlLendario.webp');
 
-            $('.premioUm').attr('src', './src/img/cards/premioUm.png');
-            $('.premioDois').attr('src', './src/img/cards/premioOuro.png');
-            $('.premioDima').attr('src', './src/img/cards/premioDima.png');
-            $('.premioMestre').attr('src', './src/img/cards/premioMestre.png');
-            $('.premioLendario').attr('src', './src/img/cards/premioLendario.png');
+            $('.premioUm').attr('src', './src/img/cards/premioUm.webp');
+            $('.premioDois').attr('src', './src/img/cards/premioOuro.webp');
+            $('.premioDima').attr('src', './src/img/cards/premioDima.webp');
+            $('.premioMestre').attr('src', './src/img/cards/premioMestre.webp');
+            $('.premioLendario').attr('src', './src/img/cards/premioLendario.webp');
 
         }
     }
